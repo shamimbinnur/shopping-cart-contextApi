4228680bf9ea2ade1c59603c2ecd483efdbb5786
Shamim Bin Nur <48399373+shamimbinnur@users.noreply.github.com>
2021-10-05 23:26:09 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+// 	AUTHOR:		SHAMIM BIN NUR
+// 	GITHUB:		https://github.com/shamimbinnur
+// 	LINKEDIN:	https://www.linkedin.com/in/shamimbinnur
+// 	MAIL:		iamshamimbn@gmail.com
+// 	SITE:		www.shamimbinnur.me
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
